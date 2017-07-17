4c38c6a470847623ded45ae6d5145b502cb56fa3
Silverio <gsilver@umich.edu>
2017-07-17 16:12:48 -0400

    adding static file reloads

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
   concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var htmlSources = ['builds/development/*.html'];
 
 var jsSources=[
   'components/scripts/rclick.js',
@@ -17,6 +18,8 @@ var jsSources=[
 
 // style.scss already has an internal imports
 var sassSources= ['components/sass/style.scss'];
+
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
   gulp.src('components/coffee/tagline.coffee')
@@ -49,6 +52,8 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function() {
@@ -58,4 +63,17 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default',['coffee','js','compass','watch', 'connect']);
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload());
+});
+
+
+gulp.task('json', function(){
+  gulp.src(jsonSources)
+    .pipe(connect.reload());
+});
+
+
+
+gulp.task('default',['html','json', 'coffee','js','compass','watch', 'connect']);
